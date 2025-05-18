656158f9f6686cdc09ff355451bd5f29e1966b91
Shashwat Tripathi <tripathishashwat2309@gmail.com>
2025-05-18 22:06:03 +0530

    download button fixed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,25 +12,31 @@ function HeroSection() {
           Privacy-first offline photo management app. Capture moments, hide
           messages, and keep your data secure.
         </p>
-      <div className="flex items-center space-x-4">
-    <button
-       onClick={() => {
-    const link = document.createElement('a');
-    link.href = '/Memolens1.0.5.apk';
-    link.setAttribute('download', 'MemoLens_v1.0.5.apk');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }}
-      className="flex items-center gap-2 bg-gradient-to-r from-[#33a4b8] to-[#097f94] 
-                 text-black font-roboto py-3 px-8 rounded-lg shadow-md
-                 hover:from-[#00d9ff] hover:to-[#33a4b8] transition duration-300"
-    >
-      <HiDownload className="w-6 h-6" />
-      Download APK
-    </button>
-    <span className="text-gray-600 font-mono select-none">v1.0.5</span>
-  </div>
+ <div className="flex flex-col items-center sm:flex-row sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 sm:px-10 md:px-0">
+  <button
+    onClick={() => {
+      const link = document.createElement('a');
+      link.href = '/Memolens1.0.5.apk';
+      link.setAttribute('download', 'MemoLens_v1.0.5.apk');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }}
+    className="
+      flex items-center gap-2 
+      bg-gradient-to-r from-[#33a4b8] to-[#097f94] 
+      text-black font-roboto py-3 px-8 rounded-lg shadow-md 
+      hover:from-[#00d9ff] hover:to-[#33a4b8] 
+      transition duration-300
+    "
+  >
+    <HiDownload className="w-6 h-6" />
+    Download APK
+  </button>
+  <span className="text-gray-600 font-mono select-none">v1.0.5</span>
+</div>
+
+
       </div>
 <div className="relative mt-10 lg:mr-80 lg:w-[400px] lg:h-[600px] w-64 min-h-[400px] mb-45 mx-auto">
         {/* First Image */}
